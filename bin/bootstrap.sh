4b09ec21dde1b0eb1c9741c0f03a2668260a4c26
Simone Pasquini <simone.pasquini@gmail.com>
2014-11-25 23:31:47 +0100

    Fixed dpkg install problem for puppet Resolve LC_ALL output error

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -3,9 +3,12 @@
 if [ ! -f /var/tmp/puppetlabs-release-precise.deb ];
 then
     wget https://apt.puppetlabs.com/puppetlabs-release-precise.deb -P /var/tmp
-    sudo dpkg -i /tmp/puppetlabs-release-precise.deb
+    sudo dpkg -i /var/tmp/puppetlabs-release-precise.deb
     sudo apt-get update
     sudo apt-get install -y puppet
 else
     echo Puppet already installed
 fi
+
+#Fix environment
+sudo sh -c 'echo LC_ALL="en_US.UTF-8" >> /etc/environment'
