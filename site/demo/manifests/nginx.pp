224caa28b2fb2a0491ae7bef529ab6af690cc15b
Simone Pasquini <simone.pasquini@gmail.com>
2014-12-01 22:54:24 +0100

    For nginx remove any apache2 package and check if nginx is running

--- site/demo/manifests/nginx.pp
+++ site/demo/manifests/nginx.pp
@@ -1,5 +1,16 @@
 class demo::nginx {
+  
+  package { 'apache2':
+    ensure => absent
+  }
+
   package { 'nginx':
-    ensure => present
+    ensure  => present,
+    require => Package['apache2']
+  }
+
+  service { 'nginx':
+    ensure => running,
+    require => Package['nginx']
   }
 }
