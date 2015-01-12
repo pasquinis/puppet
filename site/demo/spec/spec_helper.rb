4642083afc243ee209254b3557769d92e5590a58
Simone Pasquini <simone.pasquini@neomobile.com>
2015-01-12 15:07:00 +0100

    Added extended module_path and graphical format for rspec

--- site/demo/spec/spec_helper.rb
+++ site/demo/spec/spec_helper.rb
@@ -1,8 +1,12 @@
 require 'rspec-puppet'
 
 fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
+module_original_path = File.expand_path(File.join(__FILE__, '..', '..', '..', '..', 'modules'))
 
 RSpec.configure do |c|
-  c.module_path = File.join(fixture_path, 'modules')
+  c.color = true
+  c.formatter = :documentation
+  c.module_path = File.join(fixture_path, 'modules') + ':' + File.join(fixture_path, 'site')
   c.manifest_dir = File.join(fixture_path, 'manifests')
+
 end
