69eadc924fab0c3012c65561cab10595c5b6bd3a
Simone Pasquini <simone.pasquini@neomobile.com>
2015-01-12 17:20:07 +0100

    Continue demo::nginx test class, without apache2 and nginx running

--- site/demo/spec/classes/nginx_spec.rb
+++ site/demo/spec/classes/nginx_spec.rb
@@ -2,5 +2,6 @@ require 'spec_helper'
 
 describe 'demo::nginx' do
   it { should compile }
-  it { should contain_service('nginx') }
+  it { should contain_package('apache2').with_ensure('absent') }
+  it { should contain_service('nginx').with_ensure('running') }
 end
