7e9456659b53e134a5bc1f41f7a1f503c794f2d3
Simone Pasquini <simone.pasquini@neomobile.com>
2015-01-15 17:23:50 +0100

    Check if the SSH service is running

--- site/demo/spec/classes/users_spec.rb
+++ site/demo/spec/classes/users_spec.rb
@@ -3,4 +3,5 @@ require 'spec_helper'
 describe 'demo::users' do
   it { should compile.with_all_deps }
   it { should contain_user('art') }
+  it { should contain_service('ssh').with_ensure('running') }
 end
