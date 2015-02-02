b8016cb4896f69b6688d1844ae618ee70fa5b0b9
Simone Pasquini <simone.pasquini@neomobile.com>
2015-02-02 17:39:58 +0100

    Adding specific version for puppet,hiera and hiera-eyaml

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -5,16 +5,16 @@ then
     wget https://apt.puppetlabs.com/puppetlabs-release-precise.deb -P /var/tmp
     sudo dpkg -i /var/tmp/puppetlabs-release-precise.deb
     sudo apt-get update
-    sudo apt-get install -y puppet
+    sudo apt-get install -y puppet=3.7.4-1puppetlabs1
 else
     echo Puppet already installed
 fi
 
 echo "Installing hiera"
-sudo puppet resource package hiera ensure=present
+sudo puppet resource package hiera ensure=1.3.4-1puppetlabs1
 
 echo "Installing hiera-eyaml"
-sudo puppet resource package hiera-eyaml provider=gem ensure=present
+sudo puppet resource package hiera-eyaml provider=gem ensure=2.0.6
 #Fix environment
 if [ $(cat /etc/environment | grep LC_ALL|wc -l) -eq 0 ];
 then
