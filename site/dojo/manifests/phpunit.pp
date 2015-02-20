a70440b76102e1317d90261fc0245208e9fbebfd
Simone Pasquini <simone.pasquini@neomobile.com>
2015-02-20 17:35:51 +0100

    Implement phpunit install
    Implemented with Phpunit guidelines

---
+++ site/dojo/manifests/phpunit.pp
@@ -0,0 +1,7 @@
+class dojo::phpunit {
+  exec { 'install PHPUnit':
+    cwd     => '/usr/local/bin',
+    command => '/usr/bin/wget -O /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar',
+    creates => '/usr/local/bin/phpunit'
+  }
+}
