5f7fa4a176fca088332e5d34bc36d5b3ebfc2655
Simone Pasquini <simone.pasquini@neomobile.com>
2015-03-02 17:46:19 +0100

    Refactoring for add repository
    Now the file is lsbdistcodename oriented

--- site/dojo/manifests/ppa.pp
+++ site/dojo/manifests/ppa.pp
@@ -1,9 +1,7 @@
 class dojo::ppa {
-  #notify {'dojo::ppa run before Stage[apt]': }
-
   exec { 'execution of add repository for ondrej php5.6':
     command => 'add-apt-repository -y ppa:ondrej/php5-5.6',
-    creates => '/etc/apt/sources.list.d/ondrej-php5-5_6-precise.list',
+    creates => "/etc/apt/sources.list.d/ondrej-php5-5_6-${::lsbdistcodename}.list",
     notify  => Exec['apt_get_update']
   }
 }
