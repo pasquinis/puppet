5223fa582d8e908d33c1565472eda8e2923623c5
Simone Pasquini <simone.pasquini@neomobile.com>
2015-03-02 17:36:05 +0100

    Change permission to execution for phpunit

--- site/dojo/manifests/phpunit.pp
+++ site/dojo/manifests/phpunit.pp
@@ -3,5 +3,8 @@ class dojo::phpunit {
     cwd     => '/usr/local/bin',
     command => '/usr/bin/wget -O /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar',
     creates => '/usr/local/bin/phpunit'
+  } ->
+  file { '/usr/local/bin/phpunit':
+    mode => '0755'
   }
 }
