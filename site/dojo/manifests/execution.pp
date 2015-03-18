fa7873f2b86db21cb96a68cc9bfd6d9f5ee20397
Simone <simone.pasquini@neomobile.com>
2015-03-18 23:00:04 +0100

    Managed install for VIM plugins

--- site/dojo/manifests/execution.pp
+++ site/dojo/manifests/execution.pp
@@ -1,8 +1,15 @@
 class dojo::execution {
 
+  $vim_plugins_executions_flag = '/root/.install_vim_plugins'
   exec { 'I download this file only once':
     cwd     => '/tmp',
-    command => 'wget -k https://raw.githubusercontent.com/pasquinis/dotfiles/master/scripts/install_vim_plugins.sh',
+    command => "wget -k https://raw.githubusercontent.com/pasquinis/dotfiles/master/scripts/install_vim_plugins.sh",
     creates => '/tmp/install_vim_plugins.sh'
+  } ->
+  exec { 'I install all VIM plugins':
+    cwd     => '/root',
+    command => "bash /tmp/install_vim_plugins.sh && touch ${vim_plugins_executions_flag}",
+    creates => "${vim_plugins_executions_flag}"
   }
+
 }
