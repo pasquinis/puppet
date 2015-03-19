08e4d730db48a09dc86b3397b3de2a48273f1dcf
Simone Pasquini <simone.pasquini@neomobile.com>
2015-03-19 17:15:44 +0100

    Suspend Exec install install_vim_plugins
    Because it's better to do this manually

--- site/dojo/manifests/execution.pp
+++ site/dojo/manifests/execution.pp
@@ -5,8 +5,10 @@ class dojo::execution {
     cwd     => '/tmp',
     command => "wget -k https://raw.githubusercontent.com/pasquinis/dotfiles/master/scripts/install_vim_plugins.sh",
     creates => '/tmp/install_vim_plugins.sh'
-  } ->
+  }
+
   exec { 'I install all VIM plugins':
+    noop    => true,
     cwd     => '/root',
     command => "bash /tmp/install_vim_plugins.sh && touch ${vim_plugins_executions_flag}",
     creates => "${vim_plugins_executions_flag}"
