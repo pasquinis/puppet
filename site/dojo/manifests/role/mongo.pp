a53dc4767405eab6c413552b4b7348c730eea567
Simone Pasquini <simone.pasquini@neomobile.com>
2015-04-08 17:39:05 +0200

    Try to remove the mongo warning

--- site/dojo/manifests/role/mongo.pp
+++ site/dojo/manifests/role/mongo.pp
@@ -1,5 +1,14 @@
 class dojo::role::mongo (
   $list_of_packages,
+  $kernel_customization = undef
 ){
   create_resources(package, $list_of_packages)
+
+  if $kernel_customization != undef {
+    create_resources(file, $kernel_customization)
+  }
+
+  service { 'mongod':
+    ensure => running
+  }
 }
