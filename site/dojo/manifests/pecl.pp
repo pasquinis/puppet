0bddf86ed0d7d412adae3e122964cbfc14eff421
Simone Pasquini <simone.pasquini@neomobile.com>
2015-04-10 09:56:45 +0200

    Starting manage PECL

---
+++ site/dojo/manifests/pecl.pp
@@ -0,0 +1,5 @@
+class dojo::pecl (
+  $list_of_packages,
+){
+  create_resources(package, $list_of_packages)
+}
--- site/dojo/manifests/role/amp.pp
+++ site/dojo/manifests/role/amp.pp
@@ -1,4 +1,5 @@
 class dojo::role::amp {
   include dojo::role::php
+  include dojo::pecl
   include dojo::role::mongo
 }
