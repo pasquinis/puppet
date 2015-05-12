1e6fcedf3d116af30f3a34e4f40a243a35202894
Simone Pasquini <simone.pasquini@neomobile.com>
2015-05-12 23:24:59 +0200

    Removed useless script

--- site/dojo/files/wp/prepare_wp.sh
+++
@@ -1,6 +0,0 @@
-mysql -u root
-CREATE DATABASE wordpress;
-CREATE USER wordpressuser@localhost IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON wordpress.* TO wordpressuser@localhost;
-FLUSH PRIVILEGES;
-exit
