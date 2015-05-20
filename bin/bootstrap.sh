0b31b47dae0290ebd7f46559be143309e030c080
Simone Pasquini <simone.pasquini@neomobile.com>
2015-05-20 17:39:28 +0200

    Added these packages for adding APT repository

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -15,10 +15,18 @@ sudo puppet resource package hiera ensure=1.3.4-1puppetlabs1
 
 echo "Installing hiera-eyaml"
 sudo puppet resource package hiera-eyaml provider=gem ensure=2.0.6
+
+echo "Installing software-properties-common"
+sudo puppet resource package software-properties-common ensure=present
+
+echo "Installing python-software-properties"
+sudo puppet resource package python-software-properties ensure=present
+
 #Fix environment
 if [ $(cat /etc/environment | grep LC_ALL|wc -l) -eq 0 ];
 then
     sudo sh -c 'echo LC_ALL="en_US.UTF-8" >> /etc/environment'
+    sudo sh -c 'locale-gen en_US.UTF-8'
 fi
 
 #Useful for disable templatedir warning
