83bec9be0e1870316d651041f3ccd656a169dea1
Simone Pasquini <simone.pasquini@gmail.com>
2016-01-12 12:14:19 +0100

    Restored test suite

--- site/dojo/spec/classes/ruby_spec.rb
+++ site/dojo/spec/classes/ruby_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe 'dojo::ruby' do
+  let(:params) {
+    { :version => '1.9.2', :given_key => 'abcdefgh', :short_given_key => 'abcd'}
+  }
   it { should compile.with_all_deps }
 end
